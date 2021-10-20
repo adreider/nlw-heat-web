94fe8a245fceb0d953063d6d2ab0a680b0df7ec6
adreider <adreidereconomic@gmail.com>
2021-10-20 18:37:26 -0300

    first commit

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+
+import styles from './styles.module.scss';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+export function MessageList() {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    // chamada api
+    api.get<Message[]>('messages/last3').then((response) => {
+      setMessages(response.data)
+    })
+  }, [])
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map(message => (
+          <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>
+              {message.text}
+            </p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt="Adriano lino" />
+              </div>
+              <span>
+                {message.user.name}
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  )
+}
